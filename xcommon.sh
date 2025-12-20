8762fa02fe82965d0a19ddc1ae6a8e010eec93c3
Ivan <ivanvmalkov@gmail.com>
2025-12-20 12:26:02 +0100

    Update repository address and default branch

--- xcommon.sh
+++ xcommon.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 XSUPPORTEDVER=23
-XREPOADDR=https://github.com/ozonep
-XDEFBRANCH=openwrt-24.10
+XREPOADDR=https://github.com/openwrt-xiaomi
+XDEFBRANCH=xq-24.10
 
 logmsg() {
 	echo "$@"
